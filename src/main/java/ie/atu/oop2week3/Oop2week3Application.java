5dd426dc7c6de224f8f3cc563280c8a09a9e75e9
StephenDuffy-STUDENT <g00465649@atu.ie>
2026-02-04 16:17:46 +0000

    First commit

---
+++ src/main/java/ie/atu/oop2week3/Oop2week3Application.java
@@ -0,0 +1,13 @@
+package ie.atu.oop2week3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oop2week3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oop2week3Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/oop2week3/Oop2week3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.oop2week3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oop2week3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
